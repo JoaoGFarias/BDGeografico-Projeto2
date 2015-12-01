34fd7237acbfbb9032fee4d5a6b58da561862478
Edson <edbdelima@gmail.com>
2015-12-01 00:00:42 -0300

    Update st_contains.sql

--- queries/st_contains.sql
+++ queries/st_contains.sql
@@ -1,4 +1,4 @@
---faz uma consulta que mostra os distritos que contem aéroportos com a descrição militar
+-- faça uma consulta que mostra os distritos que contem aeroportos com a descrição militar
 ﻿select d.geom from afg_districts_329 as d, afg_airports_airfields as a
 <<<<<<< .merge_file_mBo7Dv
 where st_contains(d.geom, a.geom) and a.use_descri = 'Military';
