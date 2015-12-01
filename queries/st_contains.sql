f51c6b8cec18352ed63cb392da986d8f25ec0b83
leoribeiro36 <leoribeiro_20@hotmail.com>
2015-11-30 23:07:52 -0300

    Update st_contains.sql

--- queries/st_contains.sql
+++ queries/st_contains.sql
@@ -1,2 +1,3 @@
+--faz uma consulta que mostra os distritos que contem aéroportos com a descrição militar
 ﻿select d.geom from afg_districts_329 as d, afg_airports_airfields as a
 where st_contains(d.geom, a.geom) and a.use_descri = 'Military';
